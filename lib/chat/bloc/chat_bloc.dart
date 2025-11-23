7171619a800a2d5c7bb83e03eb72504401ed7ffb
Serhii Shalamov <gavrik.pixel@gmail.com>
2025-11-23 14:11:36 +0100

    ✨ feat: create ChatScreen & ChatBloc

---
+++ lib/chat/bloc/chat_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'chat_event.dart';
+part 'chat_state.dart';
+
+class ChatBloc extends Bloc<ChatEvent, ChatState> {
+  ChatBloc() : super(ChatInitial()) {
+    on<ChatEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
